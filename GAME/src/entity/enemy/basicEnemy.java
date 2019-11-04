f84c79e418646055e8a703a942087ba3338221df
dothangTH <dothang.thanhhoa@gmail.com>
2019-11-04 20:44:20 +0700

    Fixed Basic Enemy.

--- GAME/src/entity/enemy/basicEnemy.java
+++ GAME/src/entity/enemy/basicEnemy.java
@@ -5,12 +5,14 @@ public class basicEnemy extends Enemy {
     private static int      _speed       =   100;
     private static int      _reward      =   5;
     private static double   _armor       =   2.5;
+    private static int      _damage      =   5;
 
     public basicEnemy(int _X, int _Y) {
         super(_X, _Y);
         this.hitPoint = _hitPoint;
         this.speed = _speed;
         this.armor = _armor;
+        this.damage = _damage;
         this.reward = _reward;
         this.enemyType = type.BASIC;
     }
