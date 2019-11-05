2b361b531e8ab97ceac5b2b16384bce4df4f0249
AnhTuan <anhtuan081220.06@gmail.com>
2019-11-05 19:44:32 +0700

    Added "game" package and "Player" class.

--- GAME/src/game/Player.java
+++ GAME/src/game/Player.java
@@ -1,8 +1,17 @@
 package game;
 
+import entity.enemy.Enemy;
+import entity.tower.Tower;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Player {
     private static int wallet;
     private static int hitPoint;
+    Stack<Enemy> enemyList;
+    ArrayList<Tower> towerList;
 
     public static int getWallet() {
         return wallet;
@@ -13,6 +22,8 @@ public class Player {
     }
 
     public Player(int _startingWallet, int _startingHitPoint) {
+        enemyList = new Stack<Enemy>();
+        towerList = new ArrayList<Tower>();
         wallet = _startingWallet;
         hitPoint = _startingHitPoint;
     }
@@ -32,4 +43,25 @@ public class Player {
     public boolean isDefeated() {
         return (hitPoint <= 0);
     }
+
+/*    public static void main(String[] args) {
+        Player test = new Player(1000, 100);
+        test.earn(500);
+        test.earn(400);
+        Scanner input = new Scanner(System.in);
+
+        while (test.getHitPoint() > 0){
+            String player_input = input.nextLine();
+            if (player_input.equals("hurt")){
+                takeDamage(5);
+                System.out.println("Hitpoint cua em la: " + test.getHitPoint());
+            }
+            else{
+                System.out.println("Nhap lai cai gi di anh ei!!!");
+            }
+        }
+
+        System.out.println("Done!");
+
+    }*/
 }
